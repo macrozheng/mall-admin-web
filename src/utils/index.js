b2ac1fac9d49e78e53827ef91df1748a11a06a0c
zhh <zh@thinkerx.com>
2018-10-16 09:05:16 +0800

    注释修改

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,3 @@
-/**
- * Created by jiachenpan on 16/11/18.
- */
-
 export function parseTime(time, cFormat) {
   if (arguments.length === 0) {
     return null
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,7 +1,3 @@
-/**
- * Created by jiachenpan on 16/11/18.
- */
-
 export function isvalidUsername(str) {
   const valid_map = ['admin', 'test']
   return valid_map.indexOf(str.trim()) >= 0
