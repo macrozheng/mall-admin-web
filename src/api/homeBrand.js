8fb60f6aa357ea19f5788cd30825035d2bab0a06
zhh <zh@thinkerx.com>
2018-11-12 17:16:51 +0800

    添加品牌推荐功能

---
+++ src/api/homeBrand.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+export function fetchList(params) {
+  return request({
+    url:'/home/brand/list',
+    method:'get',
+    params:params
+  })
+}
+
+export function updateRecommendStatus(data) {
+  return request({
+    url:'/home/brand/update/recommendStatus',
+    method:'post',
+    data:data
+  })
+}
+
+export function deleteHomeBrand(data) {
+  return request({
+    url:'/home/brand/delete',
+    method:'post',
+    data:data
+  })
+}
+
+export function createHomeBrand(data) {
+  return request({
+    url:'/home/brand/create',
+    method:'post',
+    data:data
+  })
+}
