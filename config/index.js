03e13645307557366db0f5d15119bf165b51cb3a
LucySnappay <lucy.jin@snappay.ca>
2022-10-24 01:48:56 -0400

    v001 fix a bug in localStorage save (login.vue) add i18n (english / Chinese(sim) add slide picture verify in login add remember me switch .

--- config/index.js
+++ config/index.js
@@ -8,17 +8,17 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {},
+    "assetsSubDirectory": 'static',
+    "assetsPublicPath": '/',
+    "proxyTable": {},
 
     // Various Dev Server settings
-    host: '0.0.0.0', // can be overwritten by process.env.HOST
-    port: 8092, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
-    errorOverlay: true,
-    notifyOnErrors: true,
-    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    "host": '0.0.0.0', // can be overwritten by process.env.HOST
+    "port": 8092, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    "autoOpenBrowser": false,
+    "errorOverlay": true,
+    "notifyOnErrors": true,
+    "poll": false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
 
     /**
@@ -26,14 +26,14 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'cheap-module-eval-source-map',
+    "devtool": 'cheap-module-eval-source-map',
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
-    cacheBusting: true,
+    "cacheBusting": true,
 
-    cssSourceMap: true
+    "cssSourceMap": true
   },
 
   build: {
