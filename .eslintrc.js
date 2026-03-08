972d635d24676c087bd2984db09a3677945dd55b
naqo <2099116785@qq.com>
2026-03-08 10:03:58 +0800

    chore: 添加 ESLint 依赖

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'eslint-config-ali/typescript/vue', // 或 'alloy/typescript/vue'
+    'plugin:prettier/recommended', // 必须放在最后
+  ],
+  rules: {
+    // 自定义规则
+  },
+};
